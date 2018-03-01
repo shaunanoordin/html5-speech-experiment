8e99b29913be1de8763378372fc08e473d1bf643
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-03-01 19:18:14 +0000

    HTML page set up

--- app/main.js
+++ app/main.js
@@ -42,33 +42,27 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ function(module, exports) {
 
 	"use strict";
 
-	var _importExample = __webpack_require__(1);
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } } /*  
-	                                                                                                                                                          Starter JS
-	                                                                                                                                                          ==========
-	                                                                                                                                                          
-	                                                                                                                                                          Starter tempalte for JS projects
-	                                                                                                                                                          
-	                                                                                                                                                          (Shaun A. Noordin || shaunanoordin.com || 20160509)
-	                                                                                                                                                          ********************************************************************************
-	                                                                                                                                                           */
+	/*  
+	HTML5 Speech Experiment
+	==========
+
+	An experiment to use the still-in-development (as of Mar 2018) Web Speech API.
+
+	(Shaun A. Noordin || shaunanoordin.com || 20180301)
+	********************************************************************************
+	 */
 
 	/*  Primary App Class
 	 */
 	//==============================================================================
-
 	var App = function App() {
 	  _classCallCheck(this, App);
-
-	  var importExample = new _importExample.ImportExample("HI THERE");
-
-	  this.console = document.getElementById("console");
-	  this.console.innerHTML = "This is a starter template for JS projects. <br>" + importExample.getText();
 	};
 	//==============================================================================
 
@@ -81,45 +75,6 @@
 	window.onload = function () {
 	  window.app = new App();
 	};
-	//==============================================================================
-
-/***/ },
-/* 1 */
-/***/ function(module, exports) {
-
-	"use strict";
-
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	/*  Example Class
-	    Demonstrates ES6 import function in main.js
-	 */
-	//==============================================================================
-
-	var ImportExample = exports.ImportExample = function () {
-	  function ImportExample() {
-	    var txt = arguments.length <= 0 || arguments[0] === undefined ? "DEFAULT TEXT" : arguments[0];
-
-	    _classCallCheck(this, ImportExample);
-
-	    this.txt = txt;
-	  }
-
-	  _createClass(ImportExample, [{
-	    key: "getText",
-	    value: function getText() {
-	      return "An ImportExample class was imported and has the text: " + this.txt;
-	    }
-	  }]);
-
-	  return ImportExample;
-	}();
 	//==============================================================================
 
 /***/ }
